aaf87d436f17c790c9e97675b1fce560d3e66ab6
vanshika <kamravanshika1@gmail.com>
2024-06-12 14:24:09 +0530

    lecture 4 forked

---
+++ Lecture 4 - const avr and let/1.js
@@ -0,0 +1,16 @@
+console.log('Hello dosto hmare bank mai apka swagat hai')
+const accountId = 14443;
+let accuountEmail = "kamravanshika1@gmail.com"
+var accountPassword = "vvmby1234";
+ accountNumbersCity = "jaipur"; 
+
+// accountId = 2; not Alowwed   
+let accountState
+console.log(accountId);
+console.table([accountId, accountPassword, accuountEmail, accountNumbersCity, accountState]) 
+
+/* 1. hello dosto this is 
+out multi line comment
+2. you can change any let or var variable except const one.
+*/
+
